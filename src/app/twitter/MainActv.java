b4b0b2291a6d390e07ec9d226a845130f7374499
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-03-08 11:42:46 +0900

    m
    v-1.0
    ---
    Initial

---
+++ src/app/twitter/MainActv.java
@@ -0,0 +1,22 @@
+package app.twitter;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActv extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.actv_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_actv, menu);
+		return true;
+	}
+
+}
