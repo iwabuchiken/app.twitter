8ce3cd80a6248cf1ffb43f01bc40bf04889bae99
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-03-09 20:59:44 +0900

    D-2
    v-1.2
    ---
    1. twitter4j => changed to 3.0.5
    2. login => done

--- gen/app/twitter/R.java
+++ gen/app/twitter/R.java
@@ -27,7 +27,13 @@ public final class R {
         public static final int success=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int btnLoginTwitter=0x7f080000;
+        public static final int btnLogoutTwitter=0x7f080005;
+        public static final int btnUpdateStatus=0x7f080004;
+        public static final int lblUpdate=0x7f080002;
+        public static final int lblUserName=0x7f080001;
+        public static final int txtUpdateStatus=0x7f080003;
     }
     public static final class layout {
         public static final int actv_main=0x7f030000;
--- src/app/twitter/MainActv.java
+++ src/app/twitter/MainActv.java
@@ -4,19 +4,244 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.User;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import app.twitter.utils.AlertDialogManager;
+import app.twitter.utils.ConnectionDetector;
+
 public class MainActv extends Activity {
 
+	static String TWITTER_CONSUMER_KEY =
+					"GPH7OdrScUbYQ1RpyLkzYg";
+//	"LsCQaPOwd8k7WkyRFRZF4Q";
+	
+    static String TWITTER_CONSUMER_SECRET =
+    				"bEWx5aLKLfNQQvqHCugZC7phMDqhKPT12Uwjajbr7o";
+//    "KJbJu5IQrlwxW7Cwnax3mMzAc4j3n6Wd2dG125srgk";
+ 
+    // Preference Constants
+    static String PREFERENCE_NAME = "twitter_oauth";
+    static final String PREF_KEY_OAUTH_TOKEN = "oauth_token";
+    static final String PREF_KEY_OAUTH_SECRET = "oauth_token_secret";
+    static final String PREF_KEY_TWITTER_LOGIN = "isTwitterLogedIn";
+ 
+    static final String TWITTER_CALLBACK_URL = "oauth://t4jsample";
+ 
+    // Twitter oauth urls
+    static final String URL_TWITTER_AUTH = "auth_url";
+    static final String URL_TWITTER_OAUTH_VERIFIER = "oauth_verifier";
+    static final String URL_TWITTER_OAUTH_TOKEN = "oauth_token";
+ 
+    // Login button
+    Button btnLoginTwitter;
+    // Update status button
+    Button btnUpdateStatus;
+    // Logout button
+    Button btnLogoutTwitter;
+    // EditText for update
+    EditText txtUpdate;
+    // lbl update
+    TextView lblUpdate;
+    TextView lblUserName;
+ 
+    // Progress dialog
+    ProgressDialog pDialog;
+    
+ // Twitter
+    private static Twitter twitter;
+    private static RequestToken requestToken;
+     
+    // Shared Preferences
+    private static SharedPreferences mSharedPreferences;
+     
+    // Internet Connection detector
+    private ConnectionDetector cd;
+     
+    // Alert Dialog Manager
+    AlertDialogManager alert = new AlertDialogManager();
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.actv_main);
-	}
+		
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+         
+        cd = new ConnectionDetector(getApplicationContext());
+ 
+        // Check if Internet present
+        if (!cd.isConnectingToInternet()) {
+            // Internet Connection is not present
+            alert.showAlertDialog(MainActv.this, "Internet Connection Error",
+                    "Please connect to working Internet connection", false);
+            // stop executing code by return
+            return;
+        }
+         
+        // Check if twitter keys are set
+        if(TWITTER_CONSUMER_KEY.trim().length() == 0 || TWITTER_CONSUMER_SECRET.trim().length() == 0){
+            // Internet Connection is not present
+            alert.showAlertDialog(
+            		MainActv.this,
+            		"Twitter oAuth tokens",
+            		"Please set your twitter oauth tokens first!",
+            		false);
+            
+            // stop executing code by return
+            return;
+            
+        }
+ 
+        // All UI elements
+        btnLoginTwitter = (Button) findViewById(R.id.btnLoginTwitter);
+        btnUpdateStatus = (Button) findViewById(R.id.btnUpdateStatus);
+        btnLogoutTwitter = (Button) findViewById(R.id.btnLogoutTwitter);
+        txtUpdate = (EditText) findViewById(R.id.txtUpdateStatus);
+        lblUpdate = (TextView) findViewById(R.id.lblUpdate);
+        lblUserName = (TextView) findViewById(R.id.lblUserName);
+ 
+        // Shared Preferences
+        mSharedPreferences = getApplicationContext().getSharedPreferences(
+                "MyPref", 0);
+        
+        // Listeners
+        /**
+         * Twitter login button click event will call loginToTwitter() function
+         * */
+        btnLoginTwitter.setOnClickListener(new View.OnClickListener() {
+     
+            @Override
+            public void onClick(View arg0) {
+                // Call login twitter function
+                loginToTwitter();
+            }
+        });
+     
+        /** This if conditions is tested once is
+         * redirected from twitter page. Parse the uri to get oAuth
+         * Verifier
+         * */
+        if (!isTwitterLoggedInAlready()) {
+            Uri uri = getIntent().getData();
+            if (uri != null
+            		&& uri.toString().startsWith(TWITTER_CALLBACK_URL)) {
+                // oAuth verifier
+                String verifier = uri
+                        .getQueryParameter(URL_TWITTER_OAUTH_VERIFIER);
+     
+                try {
+                    // Get the access token
+                    AccessToken accessToken = twitter.getOAuthAccessToken(
+                            requestToken, verifier);
+     
+                    // Shared Preferences
+                    Editor e = mSharedPreferences.edit();
+     
+                    // After getting access token, access token secret
+                    // store them in application preferences
+                    e.putString(PREF_KEY_OAUTH_TOKEN, accessToken.getToken());
+                    e.putString(PREF_KEY_OAUTH_SECRET,
+                            accessToken.getTokenSecret());
+                    // Store login status - true
+                    e.putBoolean(PREF_KEY_TWITTER_LOGIN, true);
+                    e.commit(); // save changes
+     
+                    Log.e("Twitter OAuth Token", "> " + accessToken.getToken());
+     
+                    // Hide login button
+                    btnLoginTwitter.setVisibility(View.GONE);
+     
+                    // Show Update Twitter
+                    lblUpdate.setVisibility(View.VISIBLE);
+                    txtUpdate.setVisibility(View.VISIBLE);
+                    btnUpdateStatus.setVisibility(View.VISIBLE);
+                    btnLogoutTwitter.setVisibility(View.VISIBLE);
+                     
+                    // Getting user details from twitter
+                    // For now i am getting his name only
+                    long userID = accessToken.getUserId();
+                    User user = twitter.showUser(userID);
+                    String username = user.getName();
+                     
+                    // Displaying in xml ui
+                    lblUserName.setText(Html.fromHtml("<b>Welcome " + username + "</b>"));
+                    
+                } catch (Exception e) {
+                	
+                    // Check log for login errors
+                    Log.e("Twitter Login Error", "> " + e.getMessage());
+                    
+                }//try
+                
+            }//if (uri != null
+            
+        }//if (!isTwitterLoggedInAlready())
+        
+        
+	}//protected void onCreate(Bundle savedInstanceState)
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main_actv, menu);
 		return true;
+		
 	}
 
-}
+	private void loginToTwitter() {
+        // Check if already logged in
+        if (!isTwitterLoggedInAlready()) {
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.setOAuthConsumerKey(TWITTER_CONSUMER_KEY);
+            builder.setOAuthConsumerSecret(TWITTER_CONSUMER_SECRET);
+            Configuration configuration = builder.build();
+             
+            TwitterFactory factory = new TwitterFactory(configuration);
+            twitter = factory.getInstance();
+ 
+            try {
+                requestToken = twitter
+                        .getOAuthRequestToken(TWITTER_CALLBACK_URL);
+                this.startActivity(new Intent(Intent.ACTION_VIEW, Uri
+                        .parse(requestToken.getAuthenticationURL())));
+            } catch (TwitterException e) {
+                e.printStackTrace();
+            }
+        } else {
+            // user already logged into twitter
+            Toast.makeText(getApplicationContext(),
+                    "Already Logged into twitter", Toast.LENGTH_LONG).show();
+        }
+    }
+ 
+    /**
+     * Check user already logged in your application using twitter Login flag is
+     * fetched from Shared Preferences
+     * */
+    private boolean isTwitterLoggedInAlready() {
+        // return twitter login status from Shared Preferences
+        return mSharedPreferences.getBoolean(PREF_KEY_TWITTER_LOGIN, false);
+    }
+}//public class MainActv extends Activity
